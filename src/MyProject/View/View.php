efa17df7e3bdebaaf46d7788a60b92798e0f1236
Marina Sintsova <56174113+sintsovamy@users.noreply.github.com>
2023-10-31 00:12:24 +0700

    rm comments

--- src/MyProject/View/View.php
+++ src/MyProject/View/View.php
@@ -4,26 +4,20 @@ namespace MyProject\View;
 
 class View
 {
-    /** @var string */
-    private $templatesPath;
+    private string $templatesPath;
 
     public function __construct(string $templatesPath)
     {
         $this->templatesPath = $templatesPath;
     }
 
-    /**
-     * @param string $templateName
-     * @param array $vars
-     * int $code
-     */
     public function renderHtml(string $templateName, array $vars = [], int $code = 200): void
     {
         http_response_code($code);
         extract($vars);
 
         ob_start();
-  	include $this->templatesPath . '/' . $templateName;
+  	    include $this->templatesPath . '/' . $templateName;
         $buffer = ob_get_contents();
         ob_end_clean();
         echo $buffer;
