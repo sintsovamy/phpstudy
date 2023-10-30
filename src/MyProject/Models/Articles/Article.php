6b7b8ea1c2e40bafaeb9ce3267e114b5f577e36d
Marina Sintsova <56174113+sintsovamy@users.noreply.github.com>
2023-10-30 23:51:38 +0700

    rm comments

--- src/MyProject/Models/Articles/Article.php
+++ src/MyProject/Models/Articles/Article.php
@@ -8,67 +8,41 @@ use MyProject\Models\ActiveRecordEntity;
 
 class Article extends ActiveRecordEntity
 {   
-    /** @var string*/
-    protected $name;
-    /** @var string*/
-    protected $text;
-    /** @var int*/
-    protected $authorId;
-    /** @var string*/
-    protected $createdAt;
+    protected string $name;
+    protected string $text;
+    protected id $authorId;
+    protected string $createdAt;
 
-
-    /**
-     * @var User $author
-     */
     public function setAuthor(User $author): void
     {
         $this->authorId = $author->getId();
     }
 
-    /**
-     * @return string
-     */
     public function getName(): string
     {
             return $this->name;
     }
 
-    /**
-     * @return string
-     */
     public function getText(): string
     {
             return $this->text;   
     }
 
-    /**
-     * @return string
-     */
     protected static function getTableName(): string     
     {
             return 'articles';
     }
 
-    /**
-     * @return User
-     */
     public function getAuthor(): User
     {
         return User::getById($this->authorId);
     }
 
-    /**
-     * @param $text
-     */
     public function setText(string $text): void
     {
             $this->text = $text;
     }
 
-    /**
-     * @param $name
-     */
     public function setName(string $name): void
     {
             $this->name = $name;
