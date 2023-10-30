5a6ff60f7d5ca724b8901e6bb81d993a6080464b
Marina Sintsova <56174113+sintsovamy@users.noreply.github.com>
2023-10-30 22:21:00 +0700

    rm comments

--- src/MyProject/Controllers/MainController.php
+++ src/MyProject/Controllers/MainController.php
@@ -10,9 +10,7 @@ use MyProject\Exceptions\DbException;
 
 class MainController
 {
-    /** @var View */
     private View $view;
-    /** @var Db */
     private Db $db;
 
     public function __construct()
@@ -21,30 +19,17 @@ class MainController
 		$this->db = Db::getInstance();
         }
 
-    /**
-     * @param string $title
-     */
     public function main(string $title = 'Главная страница'): void
     {
-        /* @var Article $articles */
         $articles = Article::findAll();
         $this->view->renderHtml('main/main.php', ['articles' => $articles, 'title' => $title]);
         }
-
-    /**
-    * @param string $title
-    * @param string $name
-    */
 
     public function sayHello(string $name, string $title = 'Страница приветствия'): void
         {
             $this->view->renderHtml('main/hello.php', ['name' => $name, 'title' => $title]);	
         }
 
-    /**
-    * @param string $title
-    * @param string $name
-    */
     public function sayBye(string $name, string $title = 'Страница приветствия'): void
         {
             $this->view->renderHtml('main/bye.php', ['name' => $name, 'title' => $title]);
