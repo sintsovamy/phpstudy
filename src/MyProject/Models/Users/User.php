b63bffd50ce8ca7c0c2bafb2828c13422cc35480
Marina Sintsova <56174113+sintsovamy@users.noreply.github.com>
2023-10-30 23:54:19 +0700

    rm comments

--- src/MyProject/Models/Users/User.php
+++ src/MyProject/Models/Users/User.php
@@ -5,32 +5,19 @@ use MyProject\Models\ActiveRecordEntity;
 
 class User extends ActiveRecordEntity
 {
-    /** @var string*/
-    protected $nickname;
-    /** @var string*/
-    protected $email;
-    /** @var bool*/
-    protected $isConfirmed;
-    /** @var string*/
-    protected $role;
-    /** @var string*/
-    protected $passwordHash;
-    /** @var string*/
-    protected $authToken;
-    /** @var string*/
-    protected $createdAt;
+    protected string $nickname;
+    protected string $email;
+    protected bool $isConfirmed;
+    protected string $role;
+    protected string $passwordHash;
+    protected string $authToken;
+    protected string $createdAt;
 
-    /**
-     * @return string
-     */
     public function getNickName(): string
     {
         return $this->nickname;
     }
 
-    /**
-     * @return string
-     */
     protected static function getTableName(): string
 
     {
